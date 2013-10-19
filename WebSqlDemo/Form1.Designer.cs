6c4c269a51aa1f192b6beafaec677aeed3d18c02
ragha <raghadeepthi1@gmail.com>
2013-10-19 07:09:10 -0700

    Commit Changes

---
+++ WebSqlDemo/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WebSqlDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDBConnection = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDBConnection
+            // 
+            this.btnDBConnection.Location = new System.Drawing.Point(46, 53);
+            this.btnDBConnection.Name = "btnDBConnection";
+            this.btnDBConnection.Size = new System.Drawing.Size(146, 28);
+            this.btnDBConnection.TabIndex = 0;
+            this.btnDBConnection.Text = "DbConnection";
+            this.btnDBConnection.UseVisualStyleBackColor = true;
+            this.btnDBConnection.Click += new System.EventHandler(this.btnDBConnection_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnDBConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDBConnection;
+    }
+}
+
---
+++ WebSqlDemo/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WebSqlDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDBConnection_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = Helper.ConnectionString;
+            try
+            {
+                con.Open();
+                MessageBox.Show("Connected..");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("exception " + ex);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+
+            }
+        }
+    }
+}
